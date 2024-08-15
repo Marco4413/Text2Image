569529082e1c13aa78180dadec157bf9f471d3bb
Marco <46133335+Marco4413@users.noreply.github.com>
2024-08-15 12:07:41 +0200

    *MAJOR OVERHAUL* of the test script
    We using that color conversion function. The code just got 406% better.
    Productivity boosted up to 418% with modern IDEs which preview colors in strings.

--- test.py
+++ test.py
@@ -6,10 +6,15 @@ from os import path
 if __name__ == "__main__":
     out_dir = path.join(path.dirname(__file__), "test")
     font_path = path.join(path.dirname(__file__), "JetBrainsMono.ttf")
-    font = t2i.ImageFont.truetype(font_path, t2i.pt(48))
+    font = t2i.ImageFont.truetype(font_path, t2i.pt(96))
 
     t2i.generate_and_save_text_image(
         "Foo\nBar\nBaz",
         out_directory=out_dir,
         font=font,
+        fill_color=t2i.color("#ffc"),
+        background_color=t2i.color("#132"),
+        shadow_color=(0,0,0),
+        shadow_blur=10,
+        padding=(10,10),
     )
